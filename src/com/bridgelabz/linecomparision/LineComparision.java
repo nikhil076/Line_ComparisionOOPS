514c1b60770e7592183d8a08e984b33981d2746c
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-27 15:34:05 +0530

    added method to check equality of two lines

--- src/com/bridgelabz/linecomparision/LineComparision.java
+++ src/com/bridgelabz/linecomparision/LineComparision.java
@@ -6,14 +6,28 @@ public class LineComparision
 	static final int Line2x1=5,Line2x2=3,Line2y1=-5,Line2y2=3;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparision Program");
-//		calculateLineLength();
+		calculateLineLength();
 		double resultOfLine[] = calculateLineLength();
-		System.out.println(resultOfLine[0]+"  and  "+resultOfLine[1]);
+		System.out.println("The Length os line 1 is :"+resultOfLine[0]+"\nThe Length of line 2 is :"+resultOfLine[1]);
+		Double resultOfLine1 = new Double(resultOfLine[0]);
+		Double resultOfLine2 = new Double(resultOfLine[1]);
+		checkLineEquality(resultOfLine1, resultOfLine2);
 	}
 	public static double[] calculateLineLength() 
 	{
 		double LengthOfLine1 = Math.sqrt((Line1x2-Line1x1)*(Line1x2-Line1x1) + (Line1y2-Line1y1)*(Line1y2-Line1y1));
 		double LengthOfLine2 = Math.sqrt((Line2x2-Line2x1)*(Line2x2-Line2x1) + (Line2y2-Line2y1)*(Line2y2-Line2y1));
 		return new double[] {LengthOfLine1,LengthOfLine2};
+	}
+	public static void checkLineEquality(Object line1,Object line2)
+	{
+		if(line1.equals(line2))
+        {
+        	System.out.println("Both lines are equal");
+        }
+        else
+        {
+        	System.out.println("Both lines are different");
+        }
 	}
 }
